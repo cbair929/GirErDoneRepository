92d5e4d3bc6baddda5c78b19b2771d96e18b94bd
Chris Bair <cbair929@gmail.com>
2017-10-09 12:49:25 -0400

    Initial Commit
    Signed-off-by: Chris Bair <cbair929@gmail.com>

---
+++ Refactoring/src/ClassA.java
@@ -0,0 +1,15 @@
+
+public class ClassA
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassA  a = new ClassA();
+		a.methodA("default");
+		
+		ClassB b = new ClassB();
+		b.isRefactored = true;
+	}
+	public void methodA(String str) {
+		
+	}
+}
---
+++ Refactoring/src/ClassB.java
@@ -0,0 +1,6 @@
+
+public class ClassB {
+	
+	public Boolean isRefactored;
+	
+}
